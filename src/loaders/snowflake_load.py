47dda0b0900e0bdd07581dd275810afad040cb05
Gustavo <gustavocljesus@gmail.com>
2026-03-06 23:07:21 -0300

    aplica atomicity na carga.

--- src/loaders/snowflake_load.py
+++ src/loaders/snowflake_load.py
@@ -8,6 +8,14 @@ def load_incremental_data(table_name: str, max_id: int):
     with hook as sf_conn:
         with sf_conn.cursor() as sf_cursor:
             insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
-            for values in data:
-                sf_cursor.execute(insert_query, values)
+            # evitar carga incompleta dos dados devido a alguma interrupcao
+            try:
+                sf_conn.autocommit = False
+                for values in data:
+                    sf_cursor.execute(insert_query, values)
+                sf_conn.commit()
+            except Exception as e:
+                sf_conn.rollback()
+                print("Ocorreu um erro: ", e)
+                raise # relanca o erro para o airflow marcar a task como falha
                         
