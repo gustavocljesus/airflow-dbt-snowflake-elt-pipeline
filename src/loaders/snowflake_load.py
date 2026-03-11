ead815b9523a6db1387c5cb3d8e95c429f42ef4b
Gustavo <gustavocljesus@gmail.com>
2026-03-11 15:49:33 -0300

    altera max_id para last_date.

--- src/loaders/snowflake_load.py
+++ src/loaders/snowflake_load.py
@@ -1,9 +1,9 @@
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from ingestion.postgres_extractor import extract_data_postgres
 
-def load_incremental_data(table_name: str, max_id: int):
+def load_incremental_data(table_name: str, last_date: str):
     hook = SnowflakeHook(snowflake_conn_id = 'snowflake').get_conn()
-    data, columns, placeholders = extract_data_postgres(table_name, max_id)
+    data, columns, placeholders = extract_data_postgres(table_name, last_date)
     
     with hook as sf_conn:
         with sf_conn.cursor() as sf_cursor:
