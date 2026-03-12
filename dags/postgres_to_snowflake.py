7275448c3a9797010928884ca379f0c9bdb36777
Gustavo <gustavocljesus@gmail.com>
2026-03-12 17:42:32 -0300

    importa as funcoes de controle de ingestao.

--- dags/postgres_to_snowflake.py
+++ dags/postgres_to_snowflake.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from airflow.decorators import dag, task
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from src.loaders.snowflake_load import load_incremental_data
+from src.control.ingestion_control import get_last_date_ingestion, update_date_ingestion
 
 default_args = {
     'owner' : 'airflow',
@@ -33,36 +33,17 @@ def postgres_to_snowflake_elt():
     ]
     
     @task(task_id = 'get_last_date')
-    def get_last_date_ingestion(table_name: str):
-        hook = SnowflakeHook(snowflake_conn_id = 'snowflake').get_conn()
-            
-        with hook as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(f"SELECT ultima_carga FROM controle_ingestao WHERE tabela = '{table_name}'")
-                last_date = cursor.fetchone()[0]
-                return last_date if last_date is not None else datetime(2024, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
-        
+    def get_last_date(table_name: str):
+        get_last_date_ingestion(table_name)
+          
     @task(task_id = 'load_data_tables')
     def load(table_name, max_id):
         load_incremental_data(table_name, max_id)
 
     @task(task_id = 'update_control_table')
-    def update_date_ingestion(table_name: str):
-        hook = SnowflakeHook(snowflake_conn_id = 'snowflake').get_conn()
-
-        with hook as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(f"""
-                MERGE INTO controle_ingestao AS ci
-                USING (SELECT '{table_name}' AS tabela) src
-                ON ci.tabela = src.tabela
-                WHEN MATCHED THEN 
-                    UPDATE SET ci.ultima_carga = CURRENT_TIMESTAMP
-                WHEN NOT MATCHED THEN 
-                    INSERT (tabela, ultima_carga) 
-                    VALUES ('{table_name}', CURRENT_TIMESTAMP)
-            """)
-    
+    def update_control_table(table_name: str):
+        update_date_ingestion(table_name)
+          
     for table_name in table_names:
         last_date = get_last_date_ingestion(table_name)
         load(table_name, last_date)
