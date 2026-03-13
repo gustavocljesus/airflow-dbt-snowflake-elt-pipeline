983eb8c01ad446ce42f00883c81eee544c84d2b2
Gustavo <gustavocljesus@gmail.com>
2026-03-13 20:21:45 -0300

    modifica codigos sql.

--- src/control/ingestion_control.py
+++ src/control/ingestion_control.py
@@ -6,7 +6,13 @@ def get_last_date_ingestion(table_name: str):
             
         with hook as conn:
             with conn.cursor() as cursor:
-                cursor.execute(f"SELECT ultima_carga FROM controle_ingestao WHERE tabela = '{table_name}'")
+                cursor.execute(f"""
+                               SELECT ultima_carga 
+                               FROM controle_ingestao 
+                               WHERE tabela = %s 
+                               """,
+                               (table_name)
+                               )
                 last_date = cursor.fetchone()[0]
                 return last_date if last_date is not None else datetime(2024, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
 
@@ -17,11 +23,13 @@ def update_date_ingestion(table_name: str):
             with conn.cursor() as cursor:
                 cursor.execute(f"""
                 MERGE INTO controle_ingestao AS ci
-                USING (SELECT '{table_name}' AS tabela) src
+                USING (SELECT %s AS tabela) src
                 ON ci.tabela = src.tabela
                 WHEN MATCHED THEN 
-                    UPDATE SET ci.ultima_carga = CURRENT_TIMESTAMP
+                    UPDATE SET ultima_carga = CURRENT_TIMESTAMP
                 WHEN NOT MATCHED THEN 
                     INSERT (tabela, ultima_carga) 
-                    VALUES ('{table_name}', CURRENT_TIMESTAMP)
-            """)
+                    VALUES (%s, CURRENT_TIMESTAMP)
+            """,
+            (table_name, table_name)
+            )
