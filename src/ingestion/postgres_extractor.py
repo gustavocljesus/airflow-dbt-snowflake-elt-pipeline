2adb33827e46da0e64cb63029a02c419563605e9
Gustavo <gustavocljesus@gmail.com>
2026-03-13 20:29:23 -0300

    modifica consultas sql.

--- src/ingestion/postgres_extractor.py
+++ src/ingestion/postgres_extractor.py
@@ -6,12 +6,24 @@ def extract_data_postgres(table_name: str, last_date: str):
     with hook as pg_conn: 
         with pg_conn.cursor() as pg_cursor:
 
-            pg_cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
+            pg_cursor.execute(f"""
+                              SELECT column_name 
+                              FROM information_schema.columns 
+                              WHERE table_name = %s
+                              """,
+                              (table_name,)
+                              )
             columns = [row[0] for row in pg_cursor.fetchall()]
             columns_list = ', '.join(columns)
             placeholders = ', '.join(['%s'] * len(columns))
 
-            pg_cursor.execute(f"SELECT {columns_list} FROM {table_name} WHERE data_inclusao > {last_date}")
+            pg_cursor.execute(f"""
+                              SELECT {columns_list} 
+                              FROM {table_name} 
+                              WHERE data_inclusao > %s
+                              """,
+                              (last_date,)
+                              )
             rows = pg_cursor.fetchall()
 
             return rows, columns_list, placeholders
