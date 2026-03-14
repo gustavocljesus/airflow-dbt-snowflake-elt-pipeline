46942ef779e26234dfb565df17c7e769af3938fa
Gustavo <gustavocljesus@gmail.com>
2026-03-14 01:06:26 -0300

    corrige importacoes.

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 """This module contains functions for the ELT process."""
 
 from src.control.ingestion_control import get_last_date_ingestion, update_date_ingestion
-from src.ingestion.postgres_extractor import extract_data_postgres
-from src.loaders.snowflake_load import load_incremental_data
+from src.ingestion.extract.postgres_extractor import extract_data_postgres
+from src.ingestion.loaders.snowflake_load import load_incremental_data
 
 __all__ = ["get_last_date_ingestion", "update_date_ingestion", "extract_data_postgres", "load_incremental_data"]
