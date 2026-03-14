7ed50eb7af128ea684d1ee94699cf1d7148243bd
Gustavo <gustavocljesus@gmail.com>
2026-03-14 01:05:01 -0300

    modifica  arquitetura.

---
+++ src/ingestion/loaders/snowflake_load.py
@@ -0,0 +1,21 @@
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from src.ingestion.extract.postgres_extractor import extract_data_postgres
+
+def load_incremental_data(table_name: str, last_date: str):
+    hook = SnowflakeHook(snowflake_conn_id = 'snowflake').get_conn()
+    data, columns, placeholders = extract_data_postgres(table_name, last_date)
+    
+    with hook as sf_conn:
+        with sf_conn.cursor() as sf_cursor:
+            insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+            # evitar carga incompleta dos dados devido a alguma interrupcao
+            try:
+                sf_conn.autocommit = False
+                for values in data:
+                    sf_cursor.execute(insert_query, values)
+                sf_conn.commit()
+            except Exception as e:
+                sf_conn.rollback()
+                print("Ocorreu um erro: ", e)
+                raise # relanca o erro para o airflow marcar a task como falha
+                        
