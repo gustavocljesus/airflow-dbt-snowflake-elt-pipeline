7ed557a05e3032d6247632a8614a955930879506
Gustavo <gustavocljesus@gmail.com>
2026-03-21 17:27:20 -0300

    atualiza __init__.

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,6 @@
 from src.control.ingestion_control import get_last_date_ingestion, update_date_ingestion
 from src.ingestion.extract.postgres_extractor import extract_data_postgres
 from src.ingestion.loaders.snowflake_load import load_incremental_data
+from src.transform.dbt_run import transformation
 
-__all__ = ["get_last_date_ingestion", "update_date_ingestion", "extract_data_postgres", "load_incremental_data"]
+__all__ = ["get_last_date_ingestion", "update_date_ingestion", "extract_data_postgres", "load_incremental_data", "transformation"]
