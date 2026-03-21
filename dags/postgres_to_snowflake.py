4258b490afd02f57b773b9c456669aac919843a7
Gustavo <gustavocljesus@gmail.com>
2026-03-21 17:26:34 -0300

    modifica task_id das tasks.

--- dags/postgres_to_snowflake.py
+++ dags/postgres_to_snowflake.py
@@ -35,22 +35,22 @@ def postgres_to_snowflake_elt():
     
     all_done = []
 
-    @task(task_id = 'get_last_date')
+    @task
     def get_last_date(table_name: str):
         return get_last_date_ingestion(table_name)
           
-    @task(task_id = 'load_data_tables')
+    @task
     def load(table_name, last_date):
         load_incremental_data(table_name, last_date)
 
-    @task(task_id = 'update_control_table')
+    @task
     def update_control_table(table_name: str):
         update_date_ingestion(table_name)    
           
     for table_name in table_names:
-        last_date = get_last_date(table_name)
-        loaded = load(table_name, last_date)
-        updated = update_control_table(table_name)
+        last_date = get_last_date.override(task_id=f"get_last_date_{table_name}")(table_name)
+        loaded = load.override(task_id=f"load_{table_name}")(table_name, last_date)
+        updated = update_control_table.override(task_id=f"update_control_table_{table_name}")(table_name)
         all_done.append(updated)
 
     dbt_task = transformation()
