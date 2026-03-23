0a81f47dde9b5c5ce226afd2588ce74ed0175486
Gustavo <gustavocljesus@gmail.com>
2026-03-22 21:33:54 -0300

    adiciona teste.

---
+++ dbt/tests/test.sql
@@ -0,0 +1,21 @@
+-- dbt test
+WITH vendas_comparacao_preco AS (
+    SELECT
+        f.venda_id,
+        f.valor_venda,
+        d.valor_sugerido,
+        CASE
+            WHEN f.valor_venda <= d.valor_sugerido AND f.valor_venda >= d.valor_sugerido * 0.95 THEN TRUE
+            ELSE FALSE
+        END AS regra_respeitada
+    FROM {{ ref('fct_vendas') }} f
+    JOIN {{ ref('dim_veiculos') }} d 
+        ON f.veiculo_sk = d.veiculo_sk
+)
+
+SELECT 
+    venda_id, 
+    valor_venda, 
+    valor_sugerido
+FROM vendas_comparacao_preco
+WHERE regra_respeitada = FALSE
