e82cf18374f7de47d2ff981ae10389ca85e8ff94
Gustavo <gustavocljesus@gmail.com>
2026-03-22 22:15:47 -0300

    adiciona Dockerfile e requirements.

--- src/transform/dbt_run.py
+++
@@ -1,5 +0,0 @@
-from airflow.providers.standard.operators.bash import BashOperator
-
-def transformation():
-    return BashOperator(task_id="dbt_run",
-                        bash_command="cd ~/airflow/dags/dbt && dbt run && dbt test --profiles-dir ~/airflow/dags/dbt")
