c6341d61c1409abddb603adfe69fee7af9f5d609
Gustavo <gustavocljesus@gmail.com>
2026-03-22 21:02:45 -0300

    adiciona modelo de fato.

---
+++ dbt/models/facts/fct_vendas.sql
@@ -0,0 +1,37 @@
+{{ config(schema='marts', materialized='incremental', unique_key='venda_id') }}
+WITH vendas AS (
+    SELECT
+        v.id_vendas AS venda_id,
+        vei.veiculo_sk,
+        con.concessionaria_sk,
+        ve.vendedor_sk,
+        c.cliente_sk,
+        v.valor_venda, 
+        v.data_venda,
+        v.data_inclusao,
+        v.data_atualizacao
+    FROM {{ ref('stg_vendas') }} v
+    JOIN {{ ref('dim_veiculos') }} vei 
+        ON v.id_veiculos = vei.veiculo_id
+    JOIN {{ ref('dim_concessionarias') }} con 
+        ON v.id_concessionarias = con.concessionaria_id
+    JOIN {{ ref('dim_vendedores') }} ve 
+        ON v.id_vendedores = ve.vendedor_id
+    JOIN {{ ref('dim_clientes') }} c
+        ON v.id_clientes = c.cliente_id
+)
+
+SELECT
+    venda_id,
+    veiculo_sk,
+    concessionaria_sk,
+    vendedor_sk,
+    cliente_sk,
+    valor_venda,
+    data_venda,
+    data_inclusao,
+    data_atualizacao
+FROM vendas
+{% if is_incremental() %}
+    WHERE data_atualizacao > (SELECT MAX(data_atualizacao) FROM {{ this }})
+{% endif %}
