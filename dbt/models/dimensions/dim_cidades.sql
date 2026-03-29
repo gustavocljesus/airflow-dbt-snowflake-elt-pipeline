8a366c59638e9aaeb0a35ea568b9b78cd25951a6
Gustavo <gustavocljesus@gmail.com>
2026-03-29 20:48:30 -0300

    refactor: adiciona alias para referenciar colunas.

--- dbt/models/dimensions/dim_cidades.sql
+++ dbt/models/dimensions/dim_cidades.sql
@@ -1,11 +1,11 @@
 {{ config(schema='marts', materialized='table') }}
 SELECT
     {{ dbt_utils.generate_surrogate_key(['id_cidades']) }} AS cidade_sk,
-    id_cidades AS cidade_id,
-    nome_cidade,
+    c.id_cidades AS cidade_id,
+    c.nome_cidade,
     e.estado_sk,
-    data_inclusao,
-    data_atualizacao
+    c.data_inclusao,
+    c.data_atualizacao
 FROM {{ ref('stg_cidades') }} c
 JOIN {{ ref('dim_estados') }} e
     ON c.id_estados = e.estado_id
--- dbt/models/dimensions/dim_clientes.sql
+++ dbt/models/dimensions/dim_clientes.sql
@@ -1,12 +1,12 @@
 {{ config(schema='marts', materialized='table') }}
 SELECT
     {{ dbt_utils.generate_surrogate_key(['id_clientes']) }} AS cliente_sk,
-    id_clientes AS cliente_id,
-    cliente AS nome_cliente,
-    endereco,
+    c.id_clientes AS cliente_id,
+    c.cliente AS nome_cliente,
+    c.endereco,
     co.concessionaria_sk,
-    data_inclusao,
-    data_atualizacao
+    c.data_inclusao,
+    c.data_atualizacao
 FROM {{ ref('stg_clientes') }} c
 JOIN {{ ref('dim_concessionarias') }} co
     ON c.id_concessionarias = co.concessionaria_id
