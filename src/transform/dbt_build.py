660184884e925700f780305697099140ccaab31e
Gustavo <gustavocljesus@gmail.com>
2026-03-29 21:03:30 -0300

    chore: altera caminho para pasta dbt.

--- src/transform/dbt_build.py
+++ src/transform/dbt_build.py
@@ -2,4 +2,4 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 def transformation():
     return BashOperator(task_id="dbt_build",
-                        bash_command="cd ~/airflow/dags/dbt && dbt build --profiles-dir ~/airflow/dags/dbt")
+                        bash_command="cd /opt/airflow/dbt && dbt deps --profiles-dir /opt/airflow/dbt && dbt build --profiles-dir /opt/airflow/dbt")
