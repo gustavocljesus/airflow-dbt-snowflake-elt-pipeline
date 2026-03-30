73d2fa58a903bcd9ae6684764e653c3554083d86
Gustavo <gustavocljesus@gmail.com>
2026-03-29 21:15:38 -0300

    chore: adiciona __init__.py as pastas do projeto e modifica o existente.

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,1 @@
-"""This module contains functions for the ELT process."""
-
-from src.control.ingestion_control import get_last_date_ingestion, update_date_ingestion
-from src.ingestion.extract.postgres_extractor import extract_data_postgres
-from src.ingestion.loaders.snowflake_load import load_incremental_data
-from src.transform.dbt_build import transformation
-
-__all__ = ["get_last_date_ingestion", "update_date_ingestion", "extract_data_postgres", "load_incremental_data", "transformation"]
+"""ELT pipeline core package.""" 
