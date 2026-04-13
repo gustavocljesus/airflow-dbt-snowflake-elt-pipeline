cefb60bfe8ce49cdfc87d9bb18602466ebf809be
Gustavo <gustavocljesus@gmail.com>
2026-04-12 21:14:14 -0300

    fix: atualiza o caminho de execucao do dbt em Airflow bash_command.

--- src/transform/dbt_build.py
+++ src/transform/dbt_build.py
@@ -2,4 +2,4 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 def transformation():
     return BashOperator(task_id="dbt_build",
-                        bash_command="cd /opt/airflow/dbt && dbt deps --profiles-dir /opt/airflow/dbt && dbt build --profiles-dir /opt/airflow/dbt")
+                        bash_command="cd /opt/airflow/dags/dbt && dbt deps --profiles-dir /opt/airflow/dags/dbt && dbt build --profiles-dir /opt/airflow/dags/dbt")
