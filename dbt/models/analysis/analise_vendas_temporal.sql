b0e708875048b742d732e686c8f53ef358d31aad
Gustavo <gustavocljesus@gmail.com>
2026-04-26 23:20:45 -0300

    refactor: altera date_trunc de mes para dia

--- dbt/models/analysis/analise_vendas_temporal.sql
+++ dbt/models/analysis/analise_vendas_temporal.sql
@@ -1,8 +1,8 @@
 {{ config(schema='analytics', materialized='table') }}
 SELECT
-    DATE_TRUNC('month', v.data_venda_id) AS mes_venda,
+    DATE_TRUNC('day', v.data_venda_id) AS mes_venda,
     COUNT(v.venda_id) AS numero_vendas,
     SUM(v.valor_venda) AS total_vendas,
     AVG(v.valor_venda) AS valor_medio
 FROM {{ ref('fct_vendas') }} v
-GROUP BY DATE_TRUNC('month', v.data_venda_id)
+GROUP BY DATE_TRUNC('day', v.data_venda_id)
